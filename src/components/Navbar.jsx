ba4e5f25eb4258622d431f7b4fdab5b3c767a388
ketanminhas <minhasketan123@gmail.com>
2024-11-19 17:52:16 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b borer-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt='' />
+        <img onClick={() =>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt='' />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
@@ -27,7 +27,7 @@ const Navbar = () => {
                 <li className='py-1'>CONTACT</li>
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' /> 
             </NavLink>
-        </ul>
+        </ul> 
         <div className='flex item-center gap-4'>
             {
                 token 
